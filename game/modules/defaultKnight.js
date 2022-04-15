5cd916a42a2e5ebda3d8793238c61cd8e9030f09
Andrew <89749775+arethel@users.noreply.github.com>
2022-04-15 18:47:39 +0300

    commit

--- game/modules/defaultKnight.js
+++ game/modules/defaultKnight.js
@@ -1,20 +1,14 @@
 class Knight extends Hero{
 
     #wayToHero='sources/knight/knight.png'
-
-    constructor(name,player1,num){
-        super(name,player1,num)
+    name = 'knight'
+    constructor(player1,num){
+        super(player1,num)
         this.wayToHero = this.#wayToHero
         if(player===player1)
-            this.action=new Action(this,[0,1,2],'knight')
+            this.action=new Action(this,[0,1,2],this.name)
 
     }
-
-    
-
-    
-    
-
 
 }
 
